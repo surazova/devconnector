54f781858af847704b3c0bc9924ea9c89eb4b5c7
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-08 00:33:26 +0000

    Added token for access

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 //Load Usere model 
 const User = require('../../models/User');
@@ -48,4 +51,62 @@ router.post('/register', (req,res) => {
   })
 });
 
+
+
+// @route  GET request to api/users/login
+// @desc   Login User / Returning JWT Token
+// @access Public
+
+router.post('/login', (req,res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  // Find the user by email
+  User.findOne({email}).then(user => {
+    // Check for user 
+    if (!user) {
+      return res.status(404).json({email: `User not found`});
+    }
+    
+    // Check password
+    bcrypt.compare(password, user.password)  //user.password is the hashed password
+      .then(isMatch => {  // True or false
+        if(isMatch)  {//if user passed, then token is generated
+          // User Matched
+          
+          
+          const payload = { id: user.id, name: user.name, avatar: user.avatar } // Create jwt payload
+          
+          // Sign Token
+          jwt.sign(
+            payload, 
+            keys.secretOrKey, 
+            { expiresIn: 3600 }, //expires in an hour
+            (err, token) => { 
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+          });
+            
+          } else {
+          return res.status(400).json({password: `Password incorrect`});
+        }
+      });
+  });
+});
+
+// @route  GET request to api/users/current
+// @desc   Return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req,res) => {
+  //res.json(req.user);  //sending the user info through the correct token 
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email,   //without sending the password
+  })
+});
+
 module.exports = router;
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 //Routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -20,7 +21,13 @@ mongoose
     .connect(db)
     .then(() => console.log('MongoDB Connected')) //success
     .catch(err => console.log(err)); //errors
-app.get('/', (req,res) => res.send('Hello'));
+// app.get('/', (req,res) => res.send('Hello'));
+
+// Passport Middleware 
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
 
 //Use routes 
 app.use('/api/users', users);
@@ -31,3 +38,4 @@ app.use('/api/posts', posts);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
