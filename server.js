6522540a7c95dba852de040afdb84f2337f75d73
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-12 20:25:50 +0000

    Added navbar, login, and register

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // Body Parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB Config
@@ -35,7 +35,9 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 //Port
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
+const port = 8081 || 5000;
+const ip = process.env.IP || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
+app.listen(port, () => console.log(`Server running on port ${port} and ip ${ip}`));
+//app.listen(8082, () => console.log(`Server running on ${port}`))
