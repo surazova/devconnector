bb9d43aef51e613949e85cf28240958e94fe3a2d
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-14 00:44:09 +0000

    Completed clear profile functionality, Ready to implement dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 
 import { Provider } from 'react-redux'; //A reaact component that provides application with the store, which holds the state
@@ -13,6 +14,7 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -31,7 +33,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user 
     store.dispatch(logoutUser());
-    // TOFO: Clear current profile 
+    // Clear current profile 
+    store.dispatch(clearCurrentProfile());
     // Redirect to login 
     window.location.href = '/login';
   }
@@ -48,9 +51,9 @@ class App extends Component {
         <div className="container">
         <Route exact path="/register" component={ Register } />
         <Route exact path="/login" component={ Login } />
+        <Route exact path="/dashboard" component={Dashboard} />
         </div>
         <Footer />
-        <h1>My React App</h1>
       </div>
       </Router>
       </Provider>
