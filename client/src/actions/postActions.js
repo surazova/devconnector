792e89324892b98fb67109362bc141a4e9a60299
Sitora Urazova <sitoraurazova@gmail.com>
2018-12-25 15:14:43 +0000

    Added post delete

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,8 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
-  POST_LOADING
+  POST_LOADING,
+  DELETE_POST
 }
 from './types';
 
@@ -44,6 +45,25 @@ export const getPost = () => dispatch => { // asynch request, need dispatch
       })
     )
 }
+
+// Delete Post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    )
+}
+
 
 // Set loading state
 export const setPostLoading = () => {
