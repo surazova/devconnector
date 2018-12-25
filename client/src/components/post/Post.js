efd1f60a4f51765009d80f36ffd1365ce2af8aca
Sitora Urazova <sitoraurazova@gmail.com>
2018-12-25 15:40:04 +0000

    Edited post actions and reducer

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import { getPost } from '../../actions/postActions';
 
 class Post extends Component {
   render() {
@@ -10,4 +14,12 @@ class Post extends Component {
   }
 }
 
-export default Post;
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+}
+const mapStateToProps = state => ({
+  post: state.post
+})
+
+export default connect(mapStateToProps, { getPost })(Post);
